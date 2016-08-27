e62a8cb720c65963b99e5a279e9267206f36c6e9
RamAnanth <ram.ananth14@gmail.com>
2016-08-27 11:26:56 +0530

    Adding initial code

--- src/Code.js
+++ src/Code.js
@@ -1,3 +1,54 @@
-function myFunction() {
-    // Testing code push - again
+function onEdit(e){
+  
+ var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var range = e.range;
+
+  ss.setNamedRange("RecentlyEdited",range);
+ 
+
+//Changes only that cell coloruncti to red
+  
+  range.setBackground('red'); 
+  
+   
+
 }
+
+function onOpen(){
+
+ScriptApp.newTrigger('clearFormatting').
+timeBased()
+.everyMinutes(1)
+.create();
+
+
+}
+
+function clearFormatting()
+{
+var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var range=ss.getRangeByName('RecentlyEdited');
+  range.clearFormat();
+ 
+}
+
+
+function createTimeDrivenTriggers() {
+  // Trigger every hour.
+  ScriptApp.newTrigger('sendEmails')
+      .forSpreadsheet(id)//Add spreadsheet id here
+      .timeBased()
+      .everyHours(1)
+      .create();
+}
+
+
+function sendEmails() {
+  var sheet = SpreadsheetApp.getActiveSheet();
+ var emailAddress = ""  // Put recipient mail id here
+    var message = "Check link for updates "+" Add link here";       // Add link address instead of Add link here
+    var subject = "Sending updates of Facilites Spreadsheet";
+    MailApp.sendEmail(emailAddress, subject, message);
+   
+}
+
